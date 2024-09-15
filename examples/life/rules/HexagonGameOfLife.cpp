0c89a69411359b423b23d6aa1582a2096fa921de
User <User@MRBEAST.local>
2024-09-15 14:40:26 -0400

    Update HexagonGameOfLife.cpp, JohnConway.cpp, and third_party.txt

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,47 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+#include <iostream>
+
+void HexagonGameOfLife::Step(World& world) {
+  int sideSize = world.SideSize();
+  for (int y = 0;  y < sideSize; y++) {
+    for (int x = 0; x < sideSize; x++) {
+      Point2D point(x, y);
+      int aliveNeighbors = CountNeighbors(world, point);
+      bool isAlive = world.Get(point);
+      if (isAlive) {
+        if ((aliveNeighbors == 2) || (aliveNeighbors == 3))
+          world.SetNext(point, true);
+        else
+          world.SetNext(point, false);
+      } else if (!isAlive) {
+        if (aliveNeighbors == 3)
+          world.SetNext(point, true);
+        else
+          world.SetNext(point, false);
+
+      }
+    }
+  }
+  std::cout<<"HexagonGameOfLife::Step has run"<<std::endl;
+  world.SwapBuffers();
+}
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
+  int count = 0;
+  int sideSize = world.SideSize();
+
+  Point2D offsets[6] = {
+    {0, -1} , {1, 0}, // top
+    {-1, 0} , {1, 0}, // middle
+    {-1, 1} , {0, 1} // bottom
+    };
+  for (int i = 0; i < 6; i++) {
+    Point2D neighbor((point.x + offsets[i].x + sideSize) % sideSize, (point.y + offsets[i].y + sideSize) % sideSize);
+    if (world.Get(neighbor))
+      count++;
+  }
+  // std::cout << "CountNeighbors: Point(" << point.x << ", " << point.y << ") has " << count << " neighbors." << std::endl;
+  return count;
+  }
+
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -1,11 +1,44 @@
 #include "JohnConway.h"
+#include <iostream>
 
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-  // todo: implement
+  int sideSize = world.SideSize();
+  for (int y = 0;  y < sideSize; y++) {
+    for (int x = 0; x < sideSize; x++) {
+      Point2D point(x, y);
+      int aliveNeighbors = CountNeighbors(world, point);
+      bool isAlive = world.Get(point);
+      if (isAlive) {
+        if ((aliveNeighbors == 2) || (aliveNeighbors == 3))
+          world.SetNext(point, true);
+        else
+          world.SetNext(point, false);
+      } else if (!isAlive) {
+        if (aliveNeighbors == 3)
+          world.SetNext(point, true);
+        else
+          world.SetNext(point, false);
+
+      }
+    }
+  }
+  std::cout<<"JohnConway::Step has run"<<std::endl;
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  // todo: implement
-  return 0;
+  int count = 0;
+  int sideSize = world.SideSize();
+  for (int dy = -1; dy < 2; dy++) {
+    for (int dx = -1; dx < 2; dx++) {
+      if (dx == 0 && dy == 0)
+        continue;
+      Point2D neighbor((point.x + dx + sideSize) % sideSize, (point.y + dy + sideSize) % sideSize);
+      if (world.Get(neighbor))
+        count++;
+    }
+  }
+ // std::cout << "CountNeighbors: Point(" << point.x << ", " << point.y << ") has " << count << " neighbors." << std::endl;
+  return count;
 }
